aa76ba793e457221c6e0511ea37750b51f6dd5b9
semjasa83 <marceldezanna@yahoo.de>
2024-05-11 22:18:40 +0200

    set color and render squares

--- js-file/game.js
+++ js-file/game.js
@@ -1,39 +1,55 @@
 "use strict";
 
-/*let randomColor = () => {
+let value = 6;
+let colors = [];
+
+function init() {
+  checkDifficulty();
+  setDifficulty();
+  createCubes();
+}
+
+function checkDifficulty() { //done
+  let selectedDifficulty = $("button.selected");
+  value = selectedDifficulty.val();
+  console.log('schwierigkeit',value);
+}
+
+function setDifficulty() { //done
+  let buttons = document.querySelectorAll("button.difficulty");
+  buttons.forEach(button => {
+    button.addEventListener('click', function () {
+      buttons.forEach(btn => btn.classList.remove("selected"));
+      this.classList.add("selected");
+      checkDifficulty();
+      colors = [];
+      createCubes();
+    });
+  });
+
+}
+
+function createCubes() {
+  let amount = value;
+  let container = document.querySelector(".items");
+  container.innerHTML = "";
+  for (let i = 0; i < amount ; i++) {
+    let cube = document.createElement("div");
+    cube.classList.add("square");
+    let color = randomColor();
+    cube.style.backgroundColor = color;
+    colors.push(color);
+    container.appendChild(cube);
+  }
+  console.log(colors);
+  // let randomIndex = Math.floor(Math.random() * colors.length);
+  // let rgbCodeElement = document.querySelector("#rgbCode");
+  // rgbCodeElement.textContent = colors[randomIndex];
+}
+
+function randomColor() {
   let r = Math.floor(Math.random() * 256);
   let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
   return `rgb(${r},${g},${b})`;
-}*/
-
-// let amount_cubes = function () {
-// /*  switch () {
-//     case easy:
-
-//   }*/
-// }
-let value = null;
-
-function init() {
-  console.log($("button"));
-  checkDifficulty();
-  setDifficulty();
-}
-
-function checkDifficulty() {
-  let selectedDifficulty = $("button.selected");
-  value = selectedDifficulty.val();
-  console.log(value);
-  return value;
-}
-
-function setDifficulty() {
-  $("button").click(function () {
-    // Entfernen Sie die Klasse 'selected' von allen Buttons
-    $("button").removeClass("selected");
-    // Fügen Sie die Klasse 'selected' zum geklickten Button hinzu
-    $(this).addClass("selected");
-    checkDifficulty();
-  });
 }
